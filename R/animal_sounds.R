2af6a821e9feb7f981bd84213680e03c8c5903cc
FlicAnderson <Felicity.Anderson@ed.ac.uk>
2021-02-17 15:16:13 +0000

    Initial commit

---
+++ R/animal_sounds.R
@@ -0,0 +1,7 @@
+animal_sounds <- function(animal, sound) {
+  assertthat::assert_that(
+    assertthat::is.string(animal),
+    assertthat::is.string(sound)
+  )
+  paste0("The ", animal, " says ", sound, "!")
+}
